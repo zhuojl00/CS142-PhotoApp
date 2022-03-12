0b36e5096425818554a652d1d364d2b2a9870fac
zhuojl00 <72666211+zhuojl00@users.noreply.github.com>
2022-03-11 21:49:02 -0800

    permissed_user

--- project8/schema/photo.js
+++ project8/schema/photo.js
@@ -34,7 +34,8 @@ var photoSchema = new mongoose.Schema({
     date_time: {type: Date, default: Date.now}, // 	The date and time when the photo was added to the database
     user_id: mongoose.Schema.Types.ObjectId, // The ID of the user who created the photo.
     comments: [commentSchema], // Array of comment objects representing the comments made on this photo.
-    tags: [tagSchema]
+    tags: [tagSchema],
+    permitted_users: [mongoose.Schema.Types.ObjectId] // list of users
 });
 
 // the schema is useless so far
